f768f5f8e0c1c82627fd655654d1402c0f6f27f9
futureboy167 <lamngth167@gmail.com>
2023-02-18 23:04:07 +0700

    update

---
+++ app/src/main/java/com/example/sscollectorv2/MainActivity.java
@@ -0,0 +1,199 @@
+package com.example.sscollectorv2;
+
+import android.app.Activity;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.sscollectorv2.databinding.ActivityMainBinding;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.wearable.activity.WearableActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class MainActivity extends WearableActivity implements SensorEventListener {
+
+    private static final String TAG = "MainActivity";
+    private SensorManager sensorManager;
+    private Sensor accelerometer, gyroscope;
+
+    private Button btRecord;
+    private Spinner spinnerAct;
+    private EditText repetition;
+    private MediaRecorder recorder = null;
+
+    private String[] actions;
+    private String action, newName;
+    private String ax, ay, az, gx, gy, gz;
+    private String filePath, fileName, audioFileName;
+    private FileWriter writer;
+
+    private boolean record = false;
+    private boolean flagA = false;
+    private boolean flagG = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if (accelerometer != null) {
+            sensorManager.registerListener(this, accelerometer, 10000);
+            Log.d(TAG, "onCreate: Registered accelerometer listener");
+        }
+
+        gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        if (gyroscope != null) {
+            sensorManager.registerListener(this, gyroscope, 10000);
+            Log.d(TAG, "onCreate: Registered gyroscope listener");
+        }
+        btRecord = findViewById(R.id.btRecord);
+        btRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                dataCollector(arg0);
+            }
+        });
+
+        repetition = findViewById(R.id.rep);
+
+        filePath = Environment.getExternalStorageDirectory() + "/SensorData";
+        File file = new File(Environment.getExternalStorageDirectory()
+                + "/SensorData");
+
+        if (!file.exists())
+            file.mkdirs();
+
+        setAmbientEnabled();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Sensor sensor = event.sensor;
+
+        if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            flagA = true;
+            ax = String.format(Locale.ROOT,"%.4f", event.values[0]);
+            ay = String.format(Locale.ROOT,"%.4f", event.values[1]);
+            az = String.format(Locale.ROOT,"%.4f", event.values[2]);
+        }
+
+        if (sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+            flagG = true;
+            gx = String.format(Locale.ROOT,"%.4f", event.values[0]);
+            gy = String.format(Locale.ROOT,"%.4f", event.values[1]);
+            gz = String.format(Locale.ROOT,"%.4f", event.values[2]);
+        }
+
+        if (flagA && flagG) {
+            String date = String.valueOf(System.currentTimeMillis());
+            date +=  "," + ax + "," + ay + "," + az + "," + gx + "," + gy + "," + gz + "\n";
+
+            if (!date.isEmpty() && record)
+                dataReader(date);
+
+            flagA = false;
+            flagG = false;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    public void dataCollector(View arg0) {
+        String buttonText = btRecord.getText().toString();
+
+        if (buttonText.equals("Start")) {
+            newName = repetition.getText().toString().trim();
+            if (newName.equals("")){
+                Toast.makeText(getApplicationContext(), "Please input file name",
+                        Toast.LENGTH_SHORT).show();
+                return;
+                }
+            audioFileName = Environment.getExternalStorageDirectory() + "/SensorData";
+
+            Toast.makeText(getApplicationContext(), "Recording",
+                    Toast.LENGTH_SHORT).show();
+
+            Date date = new Date(System.currentTimeMillis());
+            String timeMilli = "" + date.getTime();
+            record = true;
+            startRecording();
+
+            fileName = timeMilli + "_" + newName + ".csv";
+            audioFileName += "/"+timeMilli + "_" + newName+".3gp";
+            try {
+                writer = new FileWriter(new File(filePath, fileName));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            btRecord.setText("Stop");
+        }
+
+        else if (buttonText.equals("Stop")){
+            record = false;
+            repetition.setEnabled(true);
+            btRecord.setText("Start");
+            stopRecording();
+            Toast.makeText(getApplicationContext(), "Saving",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void dataReader(String data) {
+        try {
+            writer.write(data);
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void startRecording() {
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setOutputFile(audioFileName);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            recorder.prepare();
+        } catch (IOException e) {
+            Log.e("start", "prepare() failed");
+        }
+
+        recorder.start();
+    }
+
+    private void stopRecording() {
+        recorder.stop();
+        recorder.release();
+        recorder = null;
+    }
+}
